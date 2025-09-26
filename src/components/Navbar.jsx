bc08f4aabd5eb38847c0ce69c3e91d6bdb9fb4df
Samineni-Niharika <niharikasamineni983@gmail.com>
2025-09-26 14:21:56 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
+import pic from "../assets/pic.png"
+
 export default function Navbar() {
   const [user] = useAuthState(auth);
 
@@ -11,16 +13,17 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-blue-600 text-white shadow-md p-4 flex justify-between items-center">
-      <h1 className="font-bold text-xl">Health Dashboard</h1>
+    <nav className="bg-blue-600 text-white shadow-md px-12 py-5 flex justify-between items-center">
+      {/* <h1 className="font-bold text-xl">Health Dashboard</h1> */}
+      <img src={pic} alt="" className="w-[80px] gap-4 rounded-full " />
       <div className="flex items-center space-x-6">
-        <div className="hidden md:flex space-x-6">
+        <div className="hidden text-lg md:flex w-[95%] space-x-6">
             <Link to="/" className="hover:text-gray-200 transition">Home</Link>
             <Link to="/ashaworkers" className="hover:text-gray-200 transition">ASHAWorker</Link>
             <Link to="/viewreports" className="hover:text-gray-200 transition">ViewReports</Link>
             <Link to="/hospitals" className="hover:text-gray-200 transition">Hospitals</Link>
-            <Link to="/assigned-hospitals" className="hover:text-gray-200 transition">Assigned Hospitals</Link>
-            <Link to="/red-alerts" className="hover:text-gray-200 transition">Red Alerts</Link>
+            <Link to="/assigned-hospitals" className="hover:text-gray-200 transition">AssignedReports</Link>
+            <Link to="/red-alerts" className="hover:text-gray-200 transition">Alerts</Link>
            
           </div>
         {user && (
